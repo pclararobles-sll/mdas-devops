d47df9cde8852bab77f0315af31b249b603d8604
pclararobles <pau.clara@connecthink.eu>
2019-11-12 18:56:08 +0100

    cathcup sesión 6

--- pipeline.sh
+++ pipeline.sh
@@ -5,10 +5,12 @@ sudo docker network create votingapp || true
 
 # cleanup
 sudo docker rm -f myvotingapp || true
+sudo docker rm -f myredis || true
 
 # build
 sudo docker build -t pclararobles/votingapp ./src/votingapp
-sudo docker run --name myvotingapp --network votingapp -p 8080:80 -d pclararobles/votingapp
+sudo docker run --name myredis --network votingapp -d redis 
+sudo docker run --name myvotingapp --network votingapp -p 8080:80 -e REDIS="myredis:6379" -d pclararobles/votingapp
  
 # test
 sudo docker build -t pclararobles/votingapp-test ./test
